7c74abf74c5cf2c7a3bb50bc0ba243e7490e3f00
Timothy Michalak <timothymichalak@gmail.com>
2019-04-05 16:32:41 -0500

    Subsetting & Updated Package
    Cleaned MAP data to include only our relevant variables. Included fastDummies in the list of packages.

--- R Scripts/Solved_Unsolved_Murders.R
+++ R Scripts/Solved_Unsolved_Murders.R
@@ -2,36 +2,21 @@
 setwd("/Users/timothymichalak/Documents/GitHub/CrimeSolver/Data")
 
 ## necessary packages
-#library(dplyr)
 library(sqldf)
 
 ## import data
-unsolved_murders <- read.csv("unsolvedmurders10_17.csv")
-solved_murders <- read.csv("solved_murders_individual_2010_2017.csv")
+MAP_cleared_uncleared <- read.csv("unsolvedmurders10_17.csv")
 
-## filter data
-#unsolved_murders <- subset(unsolved_murders, Solved == "No")
-#solved_murders <- subset(solved_murders, Solved == "Yes")
 
 ## define SQL strings
-unsolved_select_string <- "select substr(CNTYFIPS, 1, 
+select_string <- "select substr(CNTYFIPS, 1, 
                 instr(CNTYFIPS, ',')-1) as County, 
                 substr(CNTYFIPS, instr(CNTYFIPS, ',') + 1) 
-                as State, CNTYFIPS, State, Agentype, Solved,
-                                     Year, StateName, Month, Incident,
+                as State, CNTYFIPS, State, Solved, Agentype,
+                                     Year, StateName, Month,
                                 Situation, VicAge, VicSex, VicRace,
-                                Weapon  from unsolved_murders
-                                where Solved = 'No'"
+                                Weapon  from MAP_cleared_uncleared"
 
-solved_select_string <- "select substr(CNTYFIPS, 1, 
-                instr(CNTYFIPS, ',')-1) as County, 
-                substr(CNTYFIPS, instr(CNTYFIPS, ',') + 1) 
-                as State, CNTYFIPS, State, Agentype, Solved,
-                                     Year, StateName, Month, Incident,
-                                Situation, VicAge, VicSex, VicRace,
-                                Weapon  from solved_murders
-                                where Solved = 'Yes'"
 
 ## query subset of data 
-unsolved_murders_split <- sqldf::sqldf(select_string)
-solved_murders_split <- sqldf::sqldf(solved_select_string)
+MAP_murders_split <- sqldf::sqldf(select_string)
