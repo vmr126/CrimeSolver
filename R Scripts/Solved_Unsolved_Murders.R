4670ff14b445c839d1cd09ad075efac98c78548a
vmr126 <47196798+vmr126@users.noreply.github.com>
2019-04-05 15:35:10 -0500

    Misc. Updates to Murder Queries
    Misc. Updates to Murder Queries

--- R Scripts/Solved_Unsolved_Murders.R
+++ R Scripts/Solved_Unsolved_Murders.R
@@ -2,7 +2,7 @@
 setwd("/Users/timothymichalak/Documents/GitHub/CrimeSolver/Data")
 
 ## necessary packages
-library(dplyr)
+#library(dplyr)
 library(sqldf)
 
 ## import data
@@ -10,8 +10,8 @@ unsolved_murders <- read.csv("unsolvedmurders10_17.csv")
 solved_murders <- read.csv("solved_murders_individual_2010_2017.csv")
 
 ## filter data
-unsolved_murders <- subset(unsolved_murders, Solved == "No")
-solved_murders <- subset(solved_murders, Solved == "Yes")
+#unsolved_murders <- subset(unsolved_murders, Solved == "No")
+#solved_murders <- subset(solved_murders, Solved == "Yes")
 
 ## define SQL strings
 unsolved_select_string <- "select substr(CNTYFIPS, 1, 
@@ -20,7 +20,8 @@ unsolved_select_string <- "select substr(CNTYFIPS, 1,
                 as State, CNTYFIPS, State, Agentype, Solved,
                                      Year, StateName, Month, Incident,
                                 Situation, VicAge, VicSex, VicRace,
-                                Weapon  from unsolved_murders"
+                                Weapon  from unsolved_murders
+                                where Solved = 'No'"
 
 solved_select_string <- "select substr(CNTYFIPS, 1, 
                 instr(CNTYFIPS, ',')-1) as County, 
@@ -28,7 +29,8 @@ solved_select_string <- "select substr(CNTYFIPS, 1,
                 as State, CNTYFIPS, State, Agentype, Solved,
                                      Year, StateName, Month, Incident,
                                 Situation, VicAge, VicSex, VicRace,
-                                Weapon  from solved_murders"
+                                Weapon  from solved_murders
+                                where Solved = 'Yes'"
 
 ## query subset of data 
 unsolved_murders_split <- sqldf::sqldf(select_string)
