6321f531ec9d738fe01d3655550d7befa83ba3c3
vmr126 <47196798+vmr126@users.noreply.github.com>
2019-04-05 16:32:44 -0500

    Query Updates for Census Data Aggregation
    Update DataAggregation for Census Data to Include Race, Poverty and Education Data

--- R Scripts/DataAggregation.R
+++ R Scripts/DataAggregation.R
@@ -6,23 +6,29 @@ library(sqldf)
 
 ## import data
 Poverty_Rate <- as.data.frame(read.csv("./Data/Poverty Rate.csv", header=TRUE, sep=","))
-County_Income <- as.data.frame(read.csv("./Data/County Income Data.csv", header=TRUE, sep=","))
+#County_Income <- as.data.frame(read.csv("./Data/County Income Data.csv", header=TRUE, sep=","))
 Education <- as.data.frame(read.csv("./Data/Education data.csv", header=TRUE, sep=","))
 
 ## pull data headers/descriptions from Census files
-poverty_rate_hdrs <- t(sqldf::sqldf("select 1 from Poverty_Rate Limit 1"))
+poverty_rate_hdrs <- t(sqldf::sqldf("select * from Poverty_Rate Limit 1"))
+education_hdrs <- t(sqldf::sqldf("select * from Education Limit 1"))
 
-## define SQL strings
-poverty_rate_select_string <- "select substr(GEO_display_label, 1, instr(GEO_display_label, ',')-1) as County, 
-                              substr(GEO_display_label, instr(GEO_display_label, ',') + 1) as State, 
-                              HC02_EST_VC01, 
-                              HC02_MOE_VC01 
-                              from Poverty_Rate 
-                              Limit 5"
+census_select_string <- "select substr(ed.GEO_display_label, 1, instr(ed.GEO_display_label, ',')-1) as County, 
+                          substr(ed.GEO_display_label, instr(ed.GEO_display_label, ',') + 1) as State,  
+                          ed.HC02_EST_VC17 as ED_RATE, 
+                          pr.HC03_EST_VC01 as POV_RATE,
+                          ed.HC02_EST_VC69 as HISPANIC,
+                          ed.HC02_EST_VC65 as MULTIRACIAL,
+                          ed.HC02_EST_VC61 as OTHER,
+                          ed.HC02_EST_VC57 as PAC_ISLANDER,
+                          ed.HC02_EST_VC53 as ASIAN,
+                          ed.HC02_EST_VC49 as NATIVE_AM,
+                          ed.HC02_EST_VC45 as AFR_AM,
+                          ed.HC02_EST_VC41 as WHITE
+                        from Education ed
+                        left join Poverty_Rate pr
+                        on ed.GEO_display_label = pr.GEO_display_label"
 
-## query subset of data
-subset_pov <- sqldf::sqldf(poverty_rate_select_string)
-
-
+census_query <- sqldf::sqldf(census_select_string)
 
 
