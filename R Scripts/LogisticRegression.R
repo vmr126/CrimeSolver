46fa78058a25c345e3b7bec965ace976dcb29531
Timothy Michalak <timothymichalak@gmail.com>
2019-04-15 20:22:04 -0500

    Update LogisticRegression.R

---
+++ R Scripts/LogisticRegression.R
@@ -0,0 +1,14 @@
+master.index <- createDataPartition(
+        master$ED_RATE,
+        times = 1,
+        p = 0.8,
+        list = FALSE
+)
+master.train <- master[master.index,]
+master.test <- master[-master.index,]
+
+logistic_regression <- glm(
+        formula = Solved_No ~.,
+        family = "binomial",
+        data = master.train
+)
