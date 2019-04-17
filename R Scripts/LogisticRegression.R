c593cdd8066cff07a34ccd8fb3172b750888f1e8
Timothy Michalak <timothymichalak@gmail.com>
2019-04-17 16:48:48 -0500

    Update LogisticRegression.R

--- R Scripts/LogisticRegression.R
+++ R Scripts/LogisticRegression.R
@@ -1,21 +1,44 @@
+## To run after DataAggregation
+## Create test and training data
 master.index <- createDataPartition(
-        master$ED_RATE,
+        master.CntyStFactor$VicAge,
         times = 1,
         p = 0.8,
         list = FALSE
 )
-master.train <- master[master.index,]
-master.test <- master[-master.index,]
+master.train <- master.CntyStFactor[master.index,]
+master.test <- master.CntyStFactor[-master.index,]
 
 ## Logistic regression on 100k obs w/o CntySt
-master.CntyStFactor$CntySt <- NULL
-master.CntyStFactor.sub <- master.CntyStFactor[1:100000,]
-logistic_regression <- glm(
+master.train$CntySt <- NULL
+train.logistic_regression <- glm(
         formula = Solved_No ~.,
         family = "binomial",
-        data = master.CntyStFactor.sub
+        data = master.train
 )
-glm.prob <- predict(logistic_regression,type="response")
-glm.pred <- rep("UNSOLVED",100000)
-glm.pred[glm.prob>0.5]<-"SOLVED"
-table(glm.pred,master.CntyStFactor.sub$Solved_No)
+train.glm.prob <- predict(
+                logistic_regression,
+                type="response"
+)
+## Create empty table for confusion matrix
+train.glm.pred <- rep("UNSOLVED",nrow(master.train))
+## All predictions > 0.5 coded as SOLVED
+train.glm.pred[train.glm.prob>0.5]<-"SOLVED"
+## Confusion Matrix
+train.confusionmatrix <- table(
+                                train.glm.pred,
+                               master.train$Solved_No
+)
+## Predict test data
+test.glm.prob <- predict(
+                logistic_regression, 
+                newdata=master.test,
+                type="response"
+)
+test.glm.pred <- rep("UNSOLVED",nrow(master.test))
+test.glm.pred[test.glm.prob>0.5]<-"SOLVED"
+test.confusionmatrix <- table(
+        test.glm.pred,
+        master.test$Solved_No
+)
+
