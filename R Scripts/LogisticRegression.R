a07ba7a6132b36a817d6f6e20d4d5234f7e24994
Timothy Michalak <timothymichalak@gmail.com>
2019-04-17 16:29:09 -0500

    Update LogisticRegression.R

--- R Scripts/LogisticRegression.R
+++ R Scripts/LogisticRegression.R
@@ -7,8 +7,15 @@ master.index <- createDataPartition(
 master.train <- master[master.index,]
 master.test <- master[-master.index,]
 
+## Logistic regression on 100k obs w/o CntySt
+master.CntyStFactor$CntySt <- NULL
+master.CntyStFactor.sub <- master.CntyStFactor[1:100000,]
 logistic_regression <- glm(
         formula = Solved_No ~.,
         family = "binomial",
-        data = master
+        data = master.CntyStFactor.sub
 )
+glm.prob <- predict(logistic_regression,type="response")
+glm.pred <- rep("UNSOLVED",100000)
+glm.pred[glm.prob>0.5]<-"SOLVED"
+table(glm.pred,master.CntyStFactor.sub$Solved_No)
