6ffe4fad782566275c796b72d40aa9603b042854
vmr126 <47196798+vmr126@users.noreply.github.com>
2019-04-27 16:14:06 -0500

    Addition of Optimization Logic
    Addition of Optimization Logic

--- R Scripts/DecisionTree.R
+++ R Scripts/DecisionTree.R
@@ -17,7 +17,29 @@ murder_tree.conf_matrix = table(master.test$Solved, murder_tree.pred)
 #murder_tree.cv = xpred.rpart(murder_tree)
 #murder_tree.cv.conf_matrix = table(master.train$Solved, murder_tree.cv)
 
-rF_murder_tree = randomForest(Solved~.,data=master.train, mtry = 4, importance=TRUE, ntree=500)
+rF_murder_tree = randomForest(Solved~.,data=master.train, mtry = sqrt(ncol(master.train)), importance=TRUE, ntree=500)
 rF_murder_tree.conf_matrix = table(master.test$Solved, predict(rF_murder_tree, master.test, type="Class"))
 
 importance(rF_murder_tree)
+
+## Optimization
+customRF <- list(type = "Classification", library = "randomForest", loop = NULL)
+customRF$parameters <- data.frame(parameter = c("mtry", "ntree"), class = rep("numeric", 2), label = c("mtry", "ntree"))
+customRF$grid <- function(x, y, len = NULL, search = "grid") {}
+customRF$fit <- function(x, y, wts, param, lev, last, weights, classProbs, ...) {
+  randomForest(x, y, mtry = param$mtry, ntree=param$ntree, ...)
+}
+customRF$predict <- function(modelFit, newdata, preProc = NULL, submodels = NULL)
+  predict(modelFit, newdata)
+customRF$prob <- function(modelFit, newdata, preProc = NULL, submodels = NULL)
+  predict(modelFit, newdata, type = "prob")
+customRF$sort <- function(x) x[order(x[,1]),]
+customRF$levels <- function(x) x$classes
+
+# train model
+control <- trainControl(method="repeatedcv", number=10, repeats=3)
+tunegrid <- expand.grid(.mtry=c(1:18), .ntree=c(10, 50, 100, 250, 500))
+set.seed(26)
+custom <- train(Solved~., data=master, method=customRF, metric='Accuracy', tuneGrid=tunegrid, trControl=control)
+summary(custom)
+plot(custom)
