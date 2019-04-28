0ce616acd7377852018a5d3c26edec8fae8fcacb
vmr126 <47196798+vmr126@users.noreply.github.com>
2019-04-28 10:53:52 -0500

    Add Plots to Random Forest
    Add Plots to Random Forest

--- R Scripts/DecisionTree.R
+++ R Scripts/DecisionTree.R
@@ -17,29 +17,17 @@ murder_tree.conf_matrix = table(master.test$Solved, murder_tree.pred)
 #murder_tree.cv = xpred.rpart(murder_tree)
 #murder_tree.cv.conf_matrix = table(master.train$Solved, murder_tree.cv)
 
-rF_murder_tree = randomForest(Solved~.,data=master.train, mtry = ncol(master.train)-1, importance=TRUE, ntree=100)
+rF_murder_tree = randomForest(Solved~.,data=master, subset=sample(1:nrow(master), nrow(master)*0.8), importance=TRUE)
 rF_murder_tree.conf_matrix = table(master.test$Solved, predict(rF_murder_tree, master.test, type="Class"))
+#plot(rF_murder_tree)
+layout(matrix(c(1,2),nrow=1),
+       width=c(4,1)) 
+par(mar=c(5,4,4,0)) #No margin on the right side
+plot(rF_murder_tree, log="y")
+par(mar=c(5,0,4,2)) #No margin on the left side
+plot(c(0,1),type="n", axes=F, xlab="", ylab="")
+legend("top", colnames(rF_murder_tree$err.rate),col=1:4,cex=0.8,fill=1:4)
+
 
 importance(rF_murder_tree)
-
-## Optimization
-customRF <- list(type = "Classification", library = "randomForest", loop = NULL)
-customRF$parameters <- data.frame(parameter = c("mtry", "ntree"), class = rep("numeric", 2), label = c("mtry", "ntree"))
-customRF$grid <- function(x, y, len = NULL, search = "grid") {}
-customRF$fit <- function(x, y, wts, param, lev, last, weights, classProbs, ...) {
-  randomForest(x, y, mtry = param$mtry, ntree=param$ntree, ...)
-}
-customRF$predict <- function(modelFit, newdata, preProc = NULL, submodels = NULL)
-  predict(modelFit, newdata)
-customRF$prob <- function(modelFit, newdata, preProc = NULL, submodels = NULL)
-  predict(modelFit, newdata, type = "prob")
-customRF$sort <- function(x) x[order(x[,1]),]
-customRF$levels <- function(x) x$classes
-
-# train model
-control <- trainControl(method="repeatedcv", number=10, repeats=3)
-tunegrid <- expand.grid(.mtry=c(1:18), .ntree=c(10, 50, 100, 250, 500))
-set.seed(26)
-custom <- train(Solved~., data=master, method=customRF, metric='Accuracy', tuneGrid=tunegrid, trControl=control)
-summary(custom)
-plot(custom)
+varImpPlot(rF_murder_tree)
